1b05e2902e5eee651c91da4903e82a61f09c67f4
Muhammed Mustafa Geldi <swankydata9@gmail.com>
2023-10-23 21:07:53 +0300

    Username value added to admin panel

--- SwankyStore/custom_user/admin.py
+++ SwankyStore/custom_user/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django_use_email_as_username.admin import BaseUserAdmin
-
 from .models import User
 
 class CustomUserAdmin(BaseUserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
 
 admin.site.register(User, CustomUserAdmin)
