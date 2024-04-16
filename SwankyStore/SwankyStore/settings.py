aef8d42ed534a50773d398a3d2410751d252384e
Muhammed Mustafa Geldi <swankydata9@gmail.com>
2024-04-16 20:34:01 +0300

    Media Root url bug is fixed.

--- SwankyStore/SwankyStore/settings.py
+++ SwankyStore/SwankyStore/settings.py
@@ -118,7 +118,7 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
 MEDIA_URL = '/media/'
 
 
